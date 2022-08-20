e323f97dc0ca917a83d3be7019d13e4e64dfaa15
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2022-08-21 00:14:02 +0630

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'angular-datatable-test';
+export class AppComponent implements AfterViewInit, OnDestroy, OnInit {
+  @ViewChild(DataTableDirective, { static: false })
+  dtElement!: DataTableDirective;
+
+  dtOptions: DataTables.Settings = {};
+  data : any = [];
+
+   dtTrigger: Subject<any> = new Subject();
+
+  ngOnInit(): void {
+    this.dtOptions = {
+    };
+    this.data = [
+        {"name" : "asdfadfaf mg"},
+        {"name" : "kg kg"}
+      ];
+    console.log(this.data);
+  }
+
+  ngAfterViewInit(): void {
+    this.dtTrigger.next(this.data);
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
+  }
+
+  rerender(): void {
+    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      // Destroy the table first
+      dtInstance.destroy();
+      this.data = [
+        {"name" : "mg mg"},
+        {"name" : "kg kg"},
+        {"name" : "dfasfd dfasdf"},
+        {"name" : "af asfda"},
+      ]
+      // Call the dtTrigger to rerender again
+      this.dtTrigger.next(this.data);
+    });
+  }
+
+  rerender2(): void {
+    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      // Destroy the table first
+      dtInstance.destroy();
+      this.data = [
+        {"name" : "mgfasdfa mg"},
+        {"name" : "asdfadsadfasdfasfd dfasdf"},
+        {"name" : "sdfasdfasfdaf asfdfadsfda"},
+      ]
+      // Call the dtTrigger to rerender again
+      this.dtTrigger.next(this.data);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DataTablesModule } from 'angular-datatables';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DataTablesModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
